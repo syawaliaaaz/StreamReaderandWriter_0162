57466f33fd56f6b08a157dbe9ef5cfa9f1b61cfa
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-04 10:10:03 +0700

    Add project files.

---
+++ inFile_lia/inFile_lia.cpp
@@ -0,0 +1,14 @@
+// inFile_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    
+}
+
