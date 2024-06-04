c2c2efa93b1066febe2f4ef81da78481fa5e4d6f
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-04 10:19:28 +0700

    Menambah library fstream, iostream, dan string

--- inFile_lia/inFile_lia.cpp
+++ inFile_lia/inFile_lia.cpp
@@ -6,9 +6,35 @@
 #include <string>
 using namespace std;
 
+int main() {
+    //untuk menyimpan data berbentuk string
+    string baris;
 
-int main()
-{
-    
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+   
 }
+
 
