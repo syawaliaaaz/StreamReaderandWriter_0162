edeae80e6287aa9c40308e69c0f19e53bb878663
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-04 10:56:50 +0700

    Menambahkan library

---
+++ manualException_lia/manualException_lia.cpp
@@ -0,0 +1,24 @@
+// manualException_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "Pengecualian akan diekseskusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
+}
