c47efb822fe4a4dbe5d761e2fe9461397a4419a7
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-04 10:50:36 +0700

    Menambahkan else didalam cout dalam kondisi file tidak ditemukan

--- inFileWithPath_lia/inFileWithPath_lia.cpp
+++ inFileWithPath_lia/inFileWithPath_lia.cpp
@@ -43,18 +43,20 @@ int main() {
 
     cout << endl << ">= Membuka dan membaca file " << endl;
     //jika file ada maka
-        if (infile.is_open())
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
         {
-            //melakukan perulangan setiap baris
-            while (getline(infile, baris))
-            {
-                //dan tampilkan di sini
-                cout << baris << '\n';
-            }
-            //tutup file tersebut setelah selesai
-            infile.close();
+            //dan tampilkan di sini
+            cout << baris << '\n';
         }
-    
-
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+   
 }
 
