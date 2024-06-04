b6fce5e35055e44f098df9facd1f7a9258a78665
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-04 10:49:01 +0700

    Menggunakan looping while untuk menampilkan baris

--- inFileWithPath_lia/inFileWithPath_lia.cpp
+++ inFileWithPath_lia/inFileWithPath_lia.cpp
@@ -29,7 +29,31 @@ int main() {
         //loop akan berhenti jika anda memasukkan karakter q
         if (baris == "q") break;
         //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+        if (infile.is_open())
+        {
+            //melakukan perulangan setiap baris
+            while (getline(infile, baris))
+            {
+                //dan tampilkan di sini
+                cout << baris << '\n';
+            }
+            //tutup file tersebut setelah selesai
+            infile.close();
+        }
     
 
 }
