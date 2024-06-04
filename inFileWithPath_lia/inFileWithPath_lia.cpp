82dd5f1d64bb08ce6220ff4bcc61a54f67470871
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-04 10:38:50 +0700

    Menambah library fstream, iostream, dan string

---
+++ inFileWithPath_lia/inFileWithPath_lia.cpp
@@ -0,0 +1,34 @@
+// inFileWithPath_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+    }
+    
+}
