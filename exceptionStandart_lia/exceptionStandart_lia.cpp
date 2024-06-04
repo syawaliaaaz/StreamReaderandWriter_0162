0399b7c1f20f681dcc6ff33e5ed8c81319f52755
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-04 11:11:27 +0700

    Menambah library iostream, exception, array

---
+++ exceptionStandart_lia/exceptionStandart_lia.cpp
@@ -0,0 +1,30 @@
+// exceptionStandart_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 1, 2, 3 };
+        //pesan array integer 3 elemen
+        //cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
+}
+
