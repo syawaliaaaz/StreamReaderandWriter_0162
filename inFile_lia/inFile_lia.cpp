aaf59c669f1580d26568d7c3d30677aba6f9c27b
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-04 10:28:23 +0700

    Membuat cout untuk membuka dan membaca file

--- inFile_lia/inFile_lia.cpp
+++ inFile_lia/inFile_lia.cpp
@@ -34,9 +34,24 @@ int main() {
     ifstream infile;
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
-   
 
-
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+ 
 }
 
 
